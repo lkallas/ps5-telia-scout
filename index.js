096948492eaadabf96c16264ef5f0da75329240c
Lennar Kallas <lennar@sixfold.com>
2021-03-11 15:47:01 +0200

    E-mail notifications

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const fetch = require('node-fetch');
+const nodemailer = require('nodemailer');
 
 const MESSENTE_API_USERNAME = '<api-username-here>';
 const MESSENTE_API_PASSWORD = '<api-password-here>';
+
+// Provide your Gmail credentials
+const GMAIL_CREDENTIALS = {
+    user: '<gmail-username-here>',
+    pass: '<gmail-password-here>',
+};
+
 const SMS_NOTIFICATION_RECIPIENTS = ['+372512345678', '+372598765432']; // <-- add your SMS recipients here
+const EMAIL_NOTIFICATION_RECIPIENTS = ['email@example.com', 'john.doe@gmail.com']; // <-- add your e-mail recipients here
+
+function sendEmail(text, recipients) {
+    if (!recipients || recipients.length === 0) {
+        return;
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            ...GMAIL_CREDENTIALS,
+        },
+    });
+
+    transporter.sendMail(
+        {
+            from: GMAIL_CREDENTIALS.user,
+            to: recipients,
+            subject: 'Playstation 5',
+            text,
+            priority: 'high',
+        },
+        function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        },
+    );
+}
 
 async function sendSMS(text, recipients) {
     const baseURL = 'https://api2.messente.com/send_sms/?';
@@ -67,8 +106,9 @@ async function main() {
                 const price = prices.currentPrice.amount;
                 const url = `https://pood.telia.ee${product.url}`;
                 const message = `${displayName.et}, hind: ${price} -> ${url}`;
-                console.log(message);
                 await sendSMS(message, SMS_NOTIFICATION_RECIPIENTS);
+                sendEmail(message, EMAIL_NOTIFICATION_RECIPIENTS);
+                console.log(message);
             }
         }
     } else {
